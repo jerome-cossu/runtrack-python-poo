ce9383d27efc5d16e039820742df85850e2601b3
jerome-cossu <jerome.cossu@laplateforme.io>
2025-02-03 15:57:01 +0100

    :rocket: check

--- Jour01/job01.py
+++ Jour01/job01.py
@@ -8,5 +8,12 @@ class Operation :
     def __init__(self, nombre1 = 0, nombre2 = 0):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
-opération1 = Operation()
-print(opération1)
+
+    def addition(self) :
+        resultat = self.nombre1 + self.nombre2
+        print(f"Le résultat de l'addition est : {resultat}")
+operation1 = Operation()
+
+print(operation1)
+
+        
