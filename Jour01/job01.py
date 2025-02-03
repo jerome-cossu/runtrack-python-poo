6d8be10909f79958c0463296f04ca91224fcae08
jerome-cossu <jerome.cossu@laplateforme.io>
2025-02-03 15:42:41 +0100

    :rocket: check

--- Jour01/job01.py
+++ Jour01/job01.py
@@ -1,7 +1,12 @@
+"""Créez une classe Operation avec un constructeur (méthode qui sera appelée
+lors de l’instance de la classe). Ajoutez les attributs “nombre1” et “nombre2”
+initialisés avec des valeurs par défaut. Instanciez votre première classe et
+imprimez l’objet en console."""
+"""Résultat attendu : <__main__.Operation object at 0x0000019C06BB6BA0>"""
+
 class Operation :
     def __init__(self, nombre1 = 0, nombre2 = 0):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
 opération1 = Operation()
 print(opération1)
-    
