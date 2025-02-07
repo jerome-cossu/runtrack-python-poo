649067120e407d3eef67ff2a242262d6610b8e5c
jerome-cossu <jerome.cossu@laplateforme.io>
2025-02-07 11:06:29 +0100

    :white_check_mark: check

---
+++ jour04/job06.py
@@ -0,0 +1,30 @@
+class Vehicule:
+    def __init__(self, marque, modele, annee, prix):
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.prix = prix
+    def informationsVehicule(self):
+        print(f"Marque: {self.marque}\nModele: {self.modele}\nAnnee: {self.annee}\nPrix: {self.prix}")
+
+class Voiture(Vehicule):
+    def __init__(self, marque, modele, annee, prix):
+        super().__init__(marque, modele, annee, prix)
+        self.portes = 4
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print(f"Nombre de portes: {self.portes}")
+
+class Moto(Vehicule):
+    def __init__(self, marque, modele, annee, prix):
+        super().__init__(marque, modele, annee, prix)
+        self.roues = 2
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print(f"Nombre de roues: {self.roues}")
+
+voiture1 = Voiture("Peugeot", "208", 2020, 65000)
+voiture1.informationsVehicule()
+
+moto1 = Moto("Yamaha", "MT-07", 2019, 8000)
+moto1.informationsVehicule()
